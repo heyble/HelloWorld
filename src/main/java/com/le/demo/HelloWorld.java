0d5d0048064a687f4a5bec9baa1457090ba5cbf2
le <596202898@qq.com>
2018-10-17 16:27:32 +0800

    Import

---
+++ src/main/java/com/le/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.le.demo;
+
+/**
+ * For testing GitHub
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
